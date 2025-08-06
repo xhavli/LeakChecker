aab8a1a1a646744c41e66a50b580c9dbd58cdf37
Adam <xhavli59@stud.fit.vutbr.cz>
2025-08-06 21:58:35 +0200

    feat: Add description comments for files

--- LeakChecker/Tests/FilesDelimiters.cs
+++ LeakChecker/Tests/FilesDelimiters.cs
@@ -1,13 +1,13 @@
 namespace LeakChecker.Tests;
 
-public class FilesDelimiters
+public static class FilesDelimiters
 {
-    // [KEY] Path, [VALUE] Delimiter,    Size, Note
+    // [KEY] Path, [VALUE] Delimiter        // Size - Note - content
     public static readonly Dictionary<string, string> FilesEncodingsDictionary = new()
     {
         [@"D:Bc\Collection #2_New combo cloud_General Splits Collection_АП42.txt_1001"] = ":",  // 0.02MB - Everything in one line - raw - emails
         [@"D:Bc\2444051_2444569"] = ",",    // 0.1MB - SQL - Facebook
-        //[@"D:Bc\7_unparsedRecords"] = ":",  // 0.5MB - Might be binary - cant detect correct encoding
+        [@"D:Bc\7_unparsedRecords"] = ":",  // 0.5MB - Might be binary - cant detect correct encoding
         [@"D:Bc\1353316_1358895"] = ",",    // 1.3MB - SQL - CS ban history
         [@"D:Bc\100.txt"] = ":",            // 2MB - nickname:passwd
         [@"D:Bc\0"] = ":",                  // 5MB - Weird symbols or zeroes and hyphens in the start of the line - email:passwd
