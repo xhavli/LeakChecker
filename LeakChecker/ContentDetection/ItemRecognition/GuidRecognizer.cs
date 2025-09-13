e2c69a011378f0a2973878a860a68b27fbad0af9
Adam <xhavli59@stud.fit.vutbr.cz>
2025-09-13 23:47:32 +0200

    add: New automated recognizer for Guid and UUID. fix: New automated recognizer for Web and Uri added before

---
+++ LeakChecker/ContentDetection/ItemRecognition/GuidRecognizer.cs
@@ -0,0 +1,28 @@
+using Microsoft.Recognizers.Text.Sequence;
+
+namespace LeakChecker.ContentDetection.ItemRecognition;
+
+public static class GuidRecognizer
+{
+    private const string Culture = Microsoft.Recognizers.Text.Culture.English; // or Culture.EnglishOthers
+
+    public static Boolean TryRecognize(string line, out List<string> stringGuids, out List<Guid> guids)
+    {
+        stringGuids = new List<string>();
+        guids = new List<Guid>();
+        bool found = false;
+        
+        var results = SequenceRecognizer.RecognizeGUID(line, Culture);
+        foreach (var result in results)
+        {
+            if (Guid.TryParse(result.Text, out Guid guid))
+            {
+                stringGuids.Add(result.Text);
+                guids.Add(guid);
+                found = true;
+            }
+        }
+
+        return found;
+    }
+}
