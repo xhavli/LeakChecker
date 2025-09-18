a8bda64cf0f2c18f511349b1988b00939b163912
Adam <xhavli59@stud.fit.vutbr.cz>
2025-09-18 12:55:34 +0200

    feat: Clean code comments and add 2 mode MaritalStatuses

--- LeakChecker/ContentDetection/ItemParsing/MaritalStatusParser.cs
+++ LeakChecker/ContentDetection/ItemParsing/MaritalStatusParser.cs
@@ -2,34 +2,31 @@ namespace LeakChecker.ContentDetection.ItemParsing;
 
 public static class MaritalStatusParser
 {
-    //TODO Performance -0,0001ms per query StringComparer.OrdinalIgnoreCase but cant compare straße and strasse
     private static readonly HashSet<string> MaritalStatuses = new(StringComparer.InvariantCultureIgnoreCase)
     {
         "Single",
         "Dating",
         "Taken",
         "Partnered",
+        "In a domestic partnership",
         "In a relationship",
         "Engaged",
         "Married",
         "Separated",
         "Divorced",
         "Widowed",
+        // "It's complicated",  //TODO is it a MaritalStatus?
     };
 
-    public static bool TryParse(string token, out string? maritalStatus)
+    public static bool TryParse(string token, out string maritalStatus)
     {
-        maritalStatus = string.Empty;
-        
-        token = token.Trim();
-        if (string.IsNullOrEmpty(token)) return false;
-
         if (MaritalStatuses.Contains(token))
         {
             maritalStatus = token;
             return true;
         }
 
+        maritalStatus = string.Empty;
         return false;
     }
 }
