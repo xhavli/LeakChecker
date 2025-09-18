b4f3ae8fe93a20b83fbbb682a4f321a35b7fa06b
Adam <xhavli59@stud.fit.vutbr.cz>
2025-09-18 12:53:44 +0200

    refactor: Clean code comments

--- LeakChecker/ContentDetection/ItemParsing/GenderParser.cs
+++ LeakChecker/ContentDetection/ItemParsing/GenderParser.cs
@@ -2,47 +2,42 @@ namespace LeakChecker.ContentDetection.ItemParsing;
 
 public static class GenderParser
 {
-    //TODO Performance -0,0001ms per query StringComparer.OrdinalIgnoreCase but cant compare straße and strasse
     private static readonly HashSet<string> MaleValues = new(StringComparer.InvariantCultureIgnoreCase)
     {
-        "man", "male", "masculino", "masc", "boy"
+        "man", "male", "masculino", "masc", "boy", "he"
     };
     
-    //TODO Performance -0,0001ms per query StringComparer.OrdinalIgnoreCase but cant compare straße and strasse
     private static readonly HashSet<string> FemaleValues = new(StringComparer.InvariantCultureIgnoreCase)
     {
-        "woman", "female", "feminino", "fem", "girl"
+        "woman", "female", "feminino", "fem", "girl", "she"
     };
     
-    //TODO Performance -0,0001ms per query StringComparer.OrdinalIgnoreCase but cant compare straße and strasse
     private static readonly HashSet<string> OtherValues = new(StringComparer.InvariantCultureIgnoreCase)
     {
-        "trans", "transgender", "nonbinary", "non-binary"
+        "trans", "transgender", "nonbinary", "non-binary", "shemale"
     };
     
     public static Boolean TryParse(string token, out string gender)
     {
-        gender = string.Empty;
-
-        token = token.Trim();
-        if (string.IsNullOrEmpty(token)) return false;
-
         if (MaleValues.Contains(token))
         {
             gender = "Male";
             return true;
         }
+        
         if (FemaleValues.Contains(token))
         {
             gender = "Female";
             return true;
         }
+        
         if (OtherValues.Contains(token))
         {
             gender = "Other";
             return true;
         }
 
+        gender = string.Empty;
         return false;
     }
 }
