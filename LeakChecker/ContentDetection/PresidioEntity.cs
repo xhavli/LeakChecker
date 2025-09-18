e61484abd37c852be342bfa7c43d57252b5470f6
Adam <xhavli59@stud.fit.vutbr.cz>
2025-09-18 12:56:21 +0200

    feat: Remove unnecessary code to reach better performance

--- LeakChecker/ContentDetection/PresidioEntity.cs
+++ LeakChecker/ContentDetection/PresidioEntity.cs
@@ -13,31 +13,4 @@ public class PresidioEntity
 
     [JsonPropertyName("end")]
     public int End { get; set; }
-
-    [JsonPropertyName("score")]
-    [JsonConverter(typeof(FlexibleDoubleConverter))]
-    public double Score { get; set; }
-
-    public override string ToString()
-    {
-        return $"{Type} [{Start}-{End}], score={Score}";
-    }
-}
-
-public class FlexibleDoubleConverter : JsonConverter<double>
-{
-    public override double Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        return reader.TokenType switch
-        {
-            JsonTokenType.Number => reader.GetDouble(),
-            JsonTokenType.String => double.TryParse(reader.GetString(), out double result) ? result : 0.0,
-            _ => throw new JsonException("Unexpected token type for double")
-        };
-    }
-
-    public override void Write(Utf8JsonWriter writer, double value, JsonSerializerOptions options)
-    {
-        writer.WriteNumberValue(value);
-    }
 }
