40023ed07f585ca03f2b1a49036ecaa76dbd7a51
Adam <xhavli59@stud.fit.vutbr.cz>
2025-10-01 02:14:52 +0200

    fix: TimeStampRecognizer.cs add IsInRange validator to avoid detection of dates like year 1302. Still unclear

--- LeakChecker/ContentDetection/ItemRecognition/TimeStampRecognizer.cs
+++ LeakChecker/ContentDetection/ItemRecognition/TimeStampRecognizer.cs
@@ -5,6 +5,10 @@ namespace LeakChecker.ContentDetection.ItemRecognition;
 public static class TimeStampRecognizer
 {
     private const string Culture = Microsoft.Recognizers.Text.Culture.English; // or Culture.EnglishOthers
+    
+    // Set valid range
+    private static readonly DateTime MinDate = new DateTime(2000, 1, 1);
+    private static readonly DateTime MaxDate = DateTime.UtcNow.AddYears(10);
 
     public static Boolean TryRecognize(string line, out List<string> stringTimeStamps, out List<DateTime> timeStamps)
     {
@@ -23,7 +27,7 @@ public static class TimeStampRecognizer
             if (!value.ContainsKey("value")) continue;  // If result.Type is timerange or datetimerange, value["value"] not present in dictionary
             string dateTimeString = value["value"]; // ISO 8601 string
             
-            if (DateTime.TryParse(dateTimeString, out DateTime timeStamp))
+            if (DateTime.TryParse(dateTimeString, out DateTime timeStamp) && IsInRange(timeStamp))
             {
                 stringTimeStamps.Add(result.Text);
                 timeStamps.Add(timeStamp);
@@ -33,4 +37,7 @@ public static class TimeStampRecognizer
 
         return found;
     }
+    
+    private static bool IsInRange(DateTime dt) =>
+        dt >= MinDate && dt <= MaxDate;
 }
