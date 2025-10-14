dda53ed1e25c7bce8ff0bcdefd617f4778fd4611
Adam <xhavli59@stud.fit.vutbr.cz>
2025-10-14 17:49:02 +0200

    feat: updated Program.cs, no big changes

--- LeakChecker/Program.cs
+++ LeakChecker/Program.cs
@@ -1,10 +1,9 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
 using System.Text;
-using LeakChecker.EncodingDetection;
-using LeakChecker.ContentDetection;
-using LeakChecker.ContentDetection.RecognitionService;
-using LeakChecker.FormatDetection;
+using LeakChecker.Content.Detection.RecognitionService;
+using LeakChecker.Content.Processing;
+using LeakChecker.Format;
 using LeakChecker.Logging;
 using LeakChecker.Logging.ExecutionLogging;
 using LeakChecker.Logging.FileLogging;
@@ -27,9 +26,13 @@ public static class Program
         await pythonNerService.WaitForStart();
         
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Encoding utf8 = new UTF8Encoding(false); // false = no BOM
+        Console.OutputEncoding = utf8;  // Enforce UTF8 encoding which can handle cyrilic characters 
+        
         int success = 0;
 
-        var filePaths = FilesDelimiters.FilesEncodingsDictionary.Keys.ToList();
+        var data = FilesDelimiters.FilesDelimitersDictUtf8;
+        var filePaths = data.Keys.ToList();
         var tasks = filePaths.Select(async filePath =>
         {
             if (!File.Exists(filePath))
@@ -38,83 +41,66 @@ public static class Program
                 return;
             }
             
+            FileLogger fileLogger = await FileLogger.CreateAsync(filePath, config);
+            FileStats fileStats = new()
+            {
+                FilePath = filePath,
+                FileName = fileLogger.SubjectFileName,
+                FileBytes = fileLogger.SubjectFileBytes,
+                ProcessingStart = fileLogger.ProcessingStart,
+            };
+
             try
             {
-                FileLogger fileLogger = new FileLogger(filePath, config.LogDirectory);
-                FileStats fileStats = new()
-                {
-                    FilePath = filePath,
-                    FileName = fileLogger.SubjectFileName,
-                    FileBytes = fileLogger.SubjectFileBytes,
-                    ProcessingStart = fileLogger.ProcessingStart,
-                };
+                // TODO
+                //  EncodingDetector encDetector = new(fileLogger, fileStats);
+                //  List<EncodingSegment> encodingSegments = await encDetector.DetectFileEncodings();
+                //  fileStats.EncodingSegments = encodingSegments;
+                //  await EncodingConverter.ConvertFileToUtf8(fileLogger, encodingSegments);
                 
-                // TODO tmp unused, this is correct way which will be developed later
-                // var encodingSegments = await encodingDetector.DetectEncodingFromFilePath(file);
-                // foreach (var segment in encodingSegments.OrderBy(s => s.StartOffset))
-                // {
-                //     Encoded encoding;
-                //     await using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-                //     fileStream.Seek(segment.StartOffset, SeekOrigin.Begin);
-                //     
-                //     try
-                //     {
-                //         encoding = Encoded.GetEncoding(segment.Encoded);
-                //     }
-                //     catch (Exception e)
-                //     {
-                //         await file.Log(LogLevel.Error, e.Message);
-                //         await file.Log(LogLevel.Warning, $"Encoded set to default [{Encoded.UTF8.WebName}]");
-                //         encoding = Encoded.UTF8;
-                //     }
-                //     
-                //     using var reader = new StreamReader(fileStream, encoding, detectEncodingFromByteOrderMarks: false, leaveOpen: true);
-                //     
-                //     long bytesToRead = segment.Length;
-                //     long bytesRead = 0;
+                // TODO test this and use this
+                //  var result = DelimiterHeuristic.Analyze(filePath, maxLines: 10_000);
+                //  Console.WriteLine($"Best delimiter: {(result.BestDelimiter is '\t' ? "\\t" : result.BestDelimiter?.ToString() ?? "none")}");
+                //  Console.WriteLine($"Sampled {result.SampledLines} lines (~{result.SampledBytes} chars)");
+                //  foreach (var c in result.Candidates.Take(10))
+                //      Console.WriteLine(c);
                 //
-                //     while (bytesRead < bytesToRead)
-                //     {
-                //         var line = await reader.ReadLineAsync();
-                //         
-                //         if (line == null) continue;  //???
-                //         int readBytesByLine = encoding.GetByteCount(line);
-                //         bytesRead += readBytesByLine;
-                //         
-                //         //formatDetector.DetectDelimiter()
-                //     }
-                // }
+                //  return;
                 
-                
-                // TODO tmp used, for experimental purposes
-                Encoding encoding = await EncodingDetector.DetectEncodingFromStream(fileLogger);   // TODO delete this method
-                
-                // TODO format detector will detect pattern of content with delimiters
-                // TODO detect format like first line starts with INSERT INTO...
-                // TODO create a pattern how content looks like
                 char delimiter = await FormatDetector.DetectDelimiterFromFile(fileLogger);
-                if (delimiter == FilesDelimiters.FilesEncodingsDictionary[filePath])
+                if (delimiter == data[filePath])
                 {
                     success++;
                 }
                 else
                 {
-                    await fileLogger.Log("Detected delimiter not match", LogLevel.Warning, LogContext.Format);
-                    delimiter = FilesDelimiters.FilesEncodingsDictionary[filePath];
+                    await fileLogger.Log("Detected delimiter not match. Setting predefined", LogLevel.Warning, LogContext.Format);
+                    delimiter = data[filePath];
                 }
 
-                ContentDetector contentDetector = new(fileLogger, fileStats);
-                await contentDetector.ProcessFile(encoding, delimiter);
-                
+                ContentProcessor contentProcessor = await ContentProcessor.CreateAsync(delimiter, fileLogger, fileStats);
+                await contentProcessor.ProcessFile();
+
                 Console.WriteLine();
                 fileStats.ProcessingEnd = DateTime.Now;
                 fileStats.PrintFileStats();
                 Console.WriteLine();
-                
+
             }
             catch (Exception e)
             {
                 await execLogger.Log($"{filePath}: {e}", LogLevel.Exception, LogContext.Main);
+            }
+            finally
+            {
+                // if (!File.Exists(fileLogger.SubjectTmpFilePath))
+                // {
+                //     await execLogger.Log($"TMP File not found: {filePath}", LogLevel.Warning);
+                // }
+                // else
+                // {
+                //     File.Delete(fileLogger.SubjectTmpFilePath);
+                // }
             }
         });
 
@@ -122,8 +108,8 @@ public static class Program
         
         // pythonNerService.Stop();
         
-        await execLogger.Log($"Format success rate is {success}/{FilesDelimiters.FilesEncodingsDictionary.Keys.Count}");
-        await execLogger.Log($"Execution finished successfully. Time taken {sw.Elapsed}, current DateTime is " +
+        await execLogger.Log($"Delimiter success rate is {success}/{data.Keys.Count}");
+        await execLogger.Log($"Execution finished successfully. {data.Keys.Count} files processed. Time taken {sw.Elapsed}, current DateTime is " +
                          $"{DateTime.Now.ToString("F", CultureInfo.InvariantCulture)}", LogLevel.Success, LogContext.Main);
         await execLogger.Log("Program will exit with code 0");
         Environment.Exit(0);
