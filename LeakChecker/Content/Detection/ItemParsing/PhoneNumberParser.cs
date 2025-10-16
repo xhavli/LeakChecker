80c5af7e9924822ffaa221f378a22fc00293e9b2
Adam <xhavli59@stud.fit.vutbr.cz>
2025-10-16 20:38:08 +0200

    fix: PhoneNumberParser reject if contains letters or multiple '+'

--- LeakChecker/Content/Detection/ItemParsing/PhoneNumberParser.cs
+++ LeakChecker/Content/Detection/ItemParsing/PhoneNumberParser.cs
@@ -5,26 +5,31 @@ namespace LeakChecker.Content.Detection.ItemParsing;
 public static class PhoneNumberParser
 {
     private static readonly PhoneNumberUtil PhoneUtil = PhoneNumberUtil.GetInstance();
+    private static readonly HashSet<string> SupportedRegions = PhoneUtil.GetSupportedRegions();
     
     public static bool TryParse(string token, out string phoneNumber)
     {
         phoneNumber = string.Empty;
         
         string phoneNum = token.Replace(" ", "");
-        if (phoneNum.All(char.IsDigit) || phoneNum.StartsWith('+'))
+        
+        bool hasOnlyDigits = phoneNum.All(char.IsDigit);
+        bool hasNoLetters = !phoneNum.Any(char.IsLetter);
+        bool plusIsValid = !phoneNum.Contains('+') || phoneNum.LastIndexOf('+') == 0;
+        if (hasNoLetters && (hasOnlyDigits || plusIsValid))
         {
             string phoneNumInternational = phoneNum;
             if (!phoneNum.StartsWith('+'))
             {
                 phoneNumInternational = "+" + phoneNum;
             }
-                    
+            
             try
             {
                 var number = PhoneUtil.Parse(phoneNumInternational, null);
                 if (PhoneUtil.IsValidNumber(number))
                 {
-                    phoneNumber = phoneNumInternational;
+                    phoneNumber = PhoneUtil.Format(number, PhoneNumberFormat.E164); // International format with '+' and numbers together
                     return true;
                 }
             }
