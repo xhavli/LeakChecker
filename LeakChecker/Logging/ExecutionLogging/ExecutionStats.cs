2e28023c890ceaa73470436ce0d0b659bd73ae74
Adam <xhavli59@stud.fit.vutbr.cz>
2025-11-24 20:33:07 +0100

    refactor: Variable renaming

--- LeakChecker/Logging/ExecutionLogging/ExecutionStats.cs
+++ LeakChecker/Logging/ExecutionLogging/ExecutionStats.cs
@@ -6,11 +6,12 @@ public class ExecutionStats(Guid executionId)
 {
     public Guid ExecutionId { get; } = executionId;
     public List<Guid> FilesParsed { get; set; } = new();
-    public long BytesParsed { get; set; }
+    public long RecordsParsed { get; set; }
     public long LinesParsed { get; set; }
-    public DateTime ParsingStart { get; init; } = DateTime.Now;
-    public DateTime ParsingEnd { get; set; }
-    public TimeSpan Duration => ParsingEnd - ParsingStart;
-    public double ByteSpeed => Duration.TotalSeconds > 0 ? BytesParsed / Duration.TotalSeconds : 0;
+    public long BytesParsed { get; set; }
+    public DateTime ExecutionStart { get; init; } = DateTime.Now;
+    public DateTime ExecutionEnd { get; set; }
+    public TimeSpan Duration => ExecutionEnd - ExecutionStart;
     public double LineSpeed => Duration.TotalSeconds > 0 ? LinesParsed / Duration.TotalSeconds : 0;
+    public double ByteSpeed => Duration.TotalSeconds > 0 ? BytesParsed / Duration.TotalSeconds : 0;
 }
