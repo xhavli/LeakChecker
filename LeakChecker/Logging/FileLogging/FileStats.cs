6d9fc439ec694e1019548e30249058cb27d10d33
Adam <xhavli59@stud.fit.vutbr.cz>
2025-11-24 20:33:38 +0100

    refactor: Variables renaming

--- LeakChecker/Logging/FileLogging/FileStats.cs
+++ LeakChecker/Logging/FileLogging/FileStats.cs
@@ -6,22 +6,22 @@ namespace LeakChecker.Logging.FileLogging;
 
 public class FileStats
 {
-    public Guid ParsingId { get; init; }
+    public Guid ParseId { get; init; }
     public Guid ExecutionId { get; init; }
     public string? FileName { get; init; }
     public string? FilePath { get; init; }
     public long FileSize { get; init; }
     public long LinesRead { get; set; }
     public long BytesRead { get; set; }
-    public long RecordsCount { get; set; }
+    public long RecordsRead { get; set; }
     public Encoding? Encoding { get; set; }
     public List<EncodingSegment> EncodingSegments { get; set; } = new();
     public List<char> Delimiters { get; set; } = new();
     public List<FormatEnum> Formats { get; set; } = new();
-    public DateTime ParsingStart { get; init; } = DateTime.Now;
-    public DateTime ParsingEnd { get; set; }
-    public TimeSpan Duration => ParsingEnd - ParsingStart;
+    public DateTime ParseStart { get; init; } = DateTime.Now;
+    public DateTime ParseEnd { get; set; }
+    public TimeSpan Duration => ParseEnd - ParseStart;
     public double ByteSpeed => Duration.TotalSeconds > 0 ? BytesRead / Duration.TotalSeconds : 0;
-    public double LineSpeed => Duration.TotalSeconds > 0 ? RecordsCount / Duration.TotalSeconds : 0;
+    public double LineSpeed => Duration.TotalSeconds > 0 ? LinesRead / Duration.TotalSeconds : 0;
 }
 
