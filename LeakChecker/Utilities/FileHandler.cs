b5bc8a62e5eeb6c7c195bd9f369c0dd54480117f
Adam <xhavli59@stud.fit.vutbr.cz>
2026-02-05 19:40:16 +0100

    refactor: Remove unused code

--- LeakChecker/Utilities/FileHandler.cs
+++ LeakChecker/Utilities/FileHandler.cs
@@ -9,10 +9,20 @@ namespace LeakChecker.Utilities;
 
 public class FileHandler(ExecutionLogger logger)
 {
+    private static string? ApplicationName { get; } = AppDomain.CurrentDomain.FriendlyName;
+
     private static readonly IContentInspector MimeInspector = new ContentInspectorBuilder
     {
         Definitions = new ExhaustiveBuilder { UsageType = UsageType.PersonalNonCommercial }.Build()
     }.Build();
+    
+    private static readonly HashSet<string> SupportedMime = new(StringComparer.InvariantCultureIgnoreCase)
+    {
+        "application/xml", 
+        "application/json", 
+        "application/vnd.ms-excel", 
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    };
     
     public async Task<bool> IsAccessible(string filePath)
     {
@@ -25,45 +35,35 @@ public class FileHandler(ExecutionLogger logger)
         {
             await logger.Log($"File in path: '{filePath}' does not exist.", LogLevel.Warning);
             return false;
-            // throw new FileNotFoundException($"File in path: '{filePath}' does not exist.", filePath);
         }
         catch (UnauthorizedAccessException)
         {
-            await logger.Log($"{Program.ApplicationName} or current user does not have permission to read the file " +
+            await logger.Log($"{ApplicationName} or current user does not have permission to read the file " +
                              $"in path: '{filePath}'. Raised UnauthorizedAccessException.", LogLevel.Warning);
             return false;
-            // throw new UnauthorizedAccessException($"{Program.ApplicationName} or current user does not have permission to read the file in path: '{filePath}'. Raised UnauthorizedAccessException.");
         }
         catch (IOException)
         {
-            await logger.Log($"File in path: '{filePath}' is locked or in use. Raised IOException.", LogLevel.Warning);
+            await logger.Log($"File in path: '{filePath}' is locked, in use or its not file. Raised IOException.", LogLevel.Warning);
             return false;
-            // throw new IOException($"File in path: '{filePath}' is locked or in use. Raised IOException.");
         }
     }
     
-    public async Task<bool> IsSupported(string filePath)
+    public async Task<bool> IsSupported(string filePath, int reliableThreshold = 4000)
     {
         await using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+        
         var results = MimeInspector.Inspect(stream);
-
-        var resultsByMime = results.ByMimeType();
-        var best = resultsByMime.FirstOrDefault();
-        const int reliableThreshold = 4000;
+        var best = results.ByMimeType().FirstOrDefault();
+        
         if (best is not null && best.Points > reliableThreshold)
         {
-            bool supportedMime = best.MimeType.StartsWith("text/", StringComparison.OrdinalIgnoreCase) ||
-                                 best.MimeType.Equals("application/xml", StringComparison.OrdinalIgnoreCase) ||
-                                 best.MimeType.Equals("application/json", StringComparison.OrdinalIgnoreCase) ||
-                                 best.MimeType.Equals("application/vnd.ms-excel", StringComparison.OrdinalIgnoreCase) ||
-                                 best.MimeType.Equals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                                     StringComparison.OrdinalIgnoreCase);
-            if (!supportedMime)
+            if (!(SupportedMime.Contains(best.MimeType) || 
+                  best.MimeType.StartsWith("text/", StringComparison.OrdinalIgnoreCase)))
             {
                 await logger.Log($"File in path: '{filePath}' have MIME Extension [{best.MimeType.ToLower()}] with [{best.Points}] " +
-                                 $"success points which can't be analysed via {Program.ApplicationName}.", LogLevel.Warning);
+                                 $"success points which can't be analysed via {ApplicationName}.", LogLevel.Warning);
                 return false;
-                // throw new Exception($"File in path: '{filePath}' have MIME Extension [{best.MimeType.ToLower()}] with [{best.Points}] success points and its not supported to read in {Program.ApplicationName}.");
             }
         }
         
@@ -84,32 +84,28 @@ public class FileHandler(ExecutionLogger logger)
         }
     }
     
-    public async Task<bool> IsReadable(string filePath)
+    public async Task<bool> IsReadable(string filePath, int sampleLimit = 1000)
     {
             await using var stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
             using var reader = new StreamReader(stream);
 
             int malformed = 0;
             int sampled = 0;
-            const int sampleLimit = 1000;
             
             while (!reader.EndOfStream && sampled < sampleLimit)
             {
                 string? line = await reader.ReadLineAsync();
-                if (line == null || line.Contains('�'))
-                {
-                    malformed++;
-                }
+                if (line == null || line.Contains('�')) { malformed++; }
 
                 sampled++;
             }
             
             double successRate = sampled == 0 ? 0 : Math.Max(0, (double)(sampled - malformed) / sampled  * 100);
-            if (successRate < 0.99)
+            if (successRate < 75)
             {
-                await logger.Log($"File in path: '{filePath}' readability success rate is {successRate:N2} which is not readable.", LogLevel.Warning);
+                await logger.Log($"File in path: '{filePath}' readability success rate is {successRate:N2} which " +
+                                 $"can't be analysed via {ApplicationName}.", LogLevel.Warning);
                 return false;
-                // throw new Exception($"File in path: '{filePath}' readable success rate is {successRate:N2} which is not readable.");
             }
 
             return true;
