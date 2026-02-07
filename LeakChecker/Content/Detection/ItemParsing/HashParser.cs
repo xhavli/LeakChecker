3ecf993a6e003291e4a556de6c0fa17159dc3439
Adam <xhavli59@stud.fit.vutbr.cz>
2026-02-07 22:31:06 +0100

    feat: add some more supported hashes to HashParser

--- LeakChecker/Content/Detection/ItemParsing/HashParser.cs
+++ LeakChecker/Content/Detection/ItemParsing/HashParser.cs
@@ -61,6 +61,7 @@ public static class HashParser
         
         // SHA1
         ["SHA1"] = ItemEnum.SHA1,
+        ["sha1(CX)"] = ItemEnum.SHA1_CX,
         ["sha1(md5($pass))"] = ItemEnum.SHA1_MD5_PLAINTEXT,
         ["sha1($salt.$plaintext)"] = ItemEnum.SHA1_SALT_PLAINTEXT,
         ["sha1($plaintext.$salt)"] = ItemEnum.SHA1_PLAINTEXT_SALT,
@@ -129,7 +130,8 @@ public static class HashParser
         // MYSQL
         ["MYSQL323"] = ItemEnum.MYSQL323,
         ["MYSQL4.1/mysql5"] = ItemEnum.MYSQL41_5,
-        
+        ["MySQL $A$ (sha256crypt)"] = ItemEnum.MYSQL_A_SHA256CRYPT,
+
         // MSSQL
         ["MSSQL (2005)"] = ItemEnum.MSSQL2005,
         ["MSSQL (2012, 2014)"] = ItemEnum.MSSQL2012_2014,
@@ -154,25 +156,28 @@ public static class HashParser
         ["Electrum Wallet (Salt-Type 5)"] = ItemEnum.ELECTRUM_WALLET_5,
         
         // ETHEREUM WALLET
-        ["Ethereum Pre-Sale Wallet, PBKDF2-HMAC-SHA256"] = ItemEnum.ETHEREUM_PRE_SALE_WALLET_PBKDF2_HMAC_SHA256,
         ["Ethereum Wallet, SCRYPT"] = ItemEnum.ETHEREUM_WALLET_SCRYPT,
         ["Ethereum Wallet, PBKDF2-HMAC-SHA256"] = ItemEnum.ETHEREUM_WALLET_PBKDF2_HMAC_SHA256,
+        ["Ethereum Pre-Sale Wallet, PBKDF2-HMAC-SHA256"] = ItemEnum.ETHEREUM_PRE_SALE_WALLET_PBKDF2_HMAC_SHA256,
         
         // DJANGO
         ["Django (SHA-1)"] = ItemEnum.DJANGO_SHA1,
         ["Django (PBKDF2-SHA256)"] = ItemEnum.DJANGO_PBKDF2_SHA256,
         
         // APPLE
+        ["macOS v10.7"] = ItemEnum.MACOS_V107,
+        ["macOS v10.8+ (PBKDF2-SHA512)"] = ItemEnum.MACOS_V108_PBKDF2_SHA512,
         ["Mac OS X Keychain"] = ItemEnum.APPLE_KEYCHAIN,
         ["Apple Secure Notes"] = ItemEnum.APPLE_SECURE_NOTES,
-        ["Apple File System (APFS)"] = ItemEnum.APPLE_FILE_SYSTEM,
         ["iTunes backup < 10.0"] = ItemEnum.APPLE_ITUNES_BACKUP_BEFORE_10_0,
         ["iTunes backup >= 10.0"] = ItemEnum.APPLE_ITUNES_BACKUP_AFTER_10_0,
+        ["Apple File System (APFS)"] = ItemEnum.APPLE_FILE_SYSTEM,
         
         // MS OFFICE
         ["MS Office 2007"] = ItemEnum.MS_OFFICE_2007,
         ["MS Office 2010"] = ItemEnum.MS_OFFICE_2010,
         ["MS Office 2013"] = ItemEnum.MS_OFFICE_2013,
+        ["MS Office 2016 - SheetProtection"] = ItemEnum.MS_OFFICE_2016_SHEET_PROTECTION,
         ["MS Office <= 2003 $0/$1, MD5 + RC4"] = ItemEnum.MS_OFFICE_2003_MD5_RC4,   // Hashcat Hash-Name "MS Office ⇐ 2003 MD5 + RC4, oldoffice$0, oldoffice$1"
         ["MS Office <= 2003 $0/$1, MD5 + RC4, collider #1"] = ItemEnum.MS_OFFICE_2003_MD5_RC4_COLLIDER_1,
         ["MS Office <= 2003 $0/$1, MD5 + RC4, collider #2"] = ItemEnum.MS_OFFICE_2003_MD5_RC4_COLLIDER_2,
@@ -185,36 +190,97 @@ public static class HashParser
         ["AxCrypt in-memory SHA1"] = ItemEnum.AXCRYPT_INMEMORY_SHA1,
         
         // BITCOIN
+        // Other bitcoin hash types always fallback to Bitcoin Public Address 
         ["Bitcoin/Litecoin wallet.dat"] = ItemEnum.BITCOIN_LITTLECOIN_WALLET_DAT,
-        // TODO other bitcoin hashes have naming issues
+        ["Bitcoin Public Address"] = ItemEnum.BITCOIN_PUBLIC_ADDRESS,   // Does not exit in Hashcat Hash-Name
+        ["Bitcoin Cash Public Address"] = ItemEnum.BITCOIN_CASH_PUBLIC_ADDRESS, // Does not exit in Hashcat Hash-Name
+        ["Litecoin Public Address"] = ItemEnum.LITECOIN_PUBLIC_ADDRESS,
+        
+        // BLOCKCHAIN
+        ["Blockchain, My Wallet"] = ItemEnum.BLOCKCHAIN_MY_WALLET,
+        ["Blockchain, My wallet, v2"] = ItemEnum.BLOCKCHAIN_MY_WALLET_V2,
+        ["Blockchain, My Wallet, Second (SHA256)"] = ItemEnum.BLOCKCHAIN_MY_WALLET_SECOND_SHA256,
+        
+        // PDF
+        ["PDF 1.1 - 1.3 (Acrobat 2 - 4)"] = ItemEnum.PDF_11_13_ACROBAT_2_4,
+        ["PDF 1.1 - 1.3 (Acrobat 2 - 4), collider #1"] = ItemEnum.PDF_11_13_ACROBAT_2_4_COLLIDER_1,
+        ["PDF 1.1 - 1.3 (Acrobat 2 - 4), collider #2"] = ItemEnum.PDF_11_13_ACROBAT_2_4_COLLIDER_2,
+        ["PDF 1.4 - 1.6 (Acrobat 5 - 8)"] = ItemEnum.PDF_14_16_ACROBAT_5_8,
+        ["PDF 1.7 Level 3 (Acrobat 9)"] = ItemEnum.PDF_17_LEVEL_3_ACROBAT_9,
+        ["PDF 1.7 Level 8 (Acrobat 10 - 11)"] = ItemEnum.PDF_17_LEVEL_8_ACROBAT_10_11,
+        ["PDF Unknown Type"] = ItemEnum.PDF_UNKNOWN_TYPE,
+        
+        // KERBEROS
+        ["Kerberos 5, etype 17, DB"] = ItemEnum.KERBEROS_5_ETYPE_17_DB,
+        ["Kerberos 5, etype 18, DB"] = ItemEnum.KERBEROS_5_ETYPE_18_DB,
+        ["Kerberos 5 TGS-REP etype 23"] = ItemEnum.KERBEROS_5_TGS_REP_ETYPE_23,
+        ["Kerberos 5, etype 17, Pre-Auth"] = ItemEnum.KERBEROS_5_ETYPE_17_PRE_AUTH,
+        ["Kerberos 5, etype 18, Pre-Auth"] = ItemEnum.KERBEROS_5_ETYPE_18_PRE_AUTH,
+        ["Kerberos 5 AS-REQ Pre-Auth etype 23"] = ItemEnum.KERBEROS_5_AS_REQ_PRE_AUTH_ETYPE_23,
+        ["Kerberos 5, etype 17, TGS-REP (AES128-CTS-HMAC-SHA1-96)"] = ItemEnum.KERBEROS_5_ETYPE_17_TGS_REP_AES128_CTS_HMAC_SHA1_96,
+        ["Kerberos 5, etype 18, TGS-REP (AES256-CTS-HMAC-SHA1-96)"] = ItemEnum.KERBEROS_5_ETYPE_18_TGS_REP_AES256_CTS_HMAC_SHA1_96,
+        
+        // PKZIP
+        ["PKZIP (Compressed)"] = ItemEnum.PKZIP_COMPRESSED,
+        ["PKZIP (Uncompressed)"] = ItemEnum.PKZIP_UNCOMPRESSED,
+        ["PKZIP (Mixed Multi-File)"] = ItemEnum.PKZIP_MIXED_MULTI_FILE,
+        ["PKZIP (Compressed Multi-File)"] = ItemEnum.PKZIP_COMPRESSED_MULTI_FILE,
+        ["PKZIP (Compressed Multi-File Checksum-Only)"] = ItemEnum.PKZIP_MIXED_MULTI_FILE_CHECKSUM_ONLY,
+        
+        // RSA/DSA/EC/OPENSSH
+        ["RSA/DSA/EC/OpenSSH Private Keys ($0$)"] = ItemEnum.RSA_DSA_EC_OPENSSH_PRIVATE_KEYS_0,
+        ["RSA/DSA/EC/OpenSSH Private Keys ($1, $3$)"] = ItemEnum.RSA_DSA_EC_OPENSSH_PRIVATE_KEYS_1_3,
+        ["RSA/DSA/EC/OpenSSH Private Keys ($4$)"] = ItemEnum.RSA_DSA_EC_OPENSSH_PRIVATE_KEYS_4,
+        ["RSA/DSA/EC/OpenSSH Private Keys ($5$)"] = ItemEnum.RSA_DSA_EC_OPENSSH_PRIVATE_KEYS_5,
+        ["RSA/DSA/EC/OpenSSH Private Keys ($6$)"] = ItemEnum.RSA_DSA_EC_OPENSSH_PRIVATE_KEYS_6,
         
         // OTHERS
+        ["PHPS"] = ItemEnum.PHPS,
         ["JWT (Json Web Token)"] = ItemEnum.JWT,
         ["RAR5"] = ItemEnum.RAR5,
+        ["RAR3-hp (*0* only)"] = ItemEnum.RAR3_HP,
+        ["RAR3-p (Compressed)"] = ItemEnum.RAR3_P_COMPRESSED,
+        ["RAR3-p (Uncompressed)"] = ItemEnum.RAR3_P_UNCOMPRESSED,
         ["7-Zip"] = ItemEnum.SEVENZIP,
         ["WinZip"] = ItemEnum.WINZIP,
         ["scrypt"] = ItemEnum.SCRYPT,
         ["Drupal7"] = ItemEnum.DRUPAL7,
+        ["Radmin2"] = ItemEnum.RADMIN2,
+        ["Radmin3"] = ItemEnum.RADMIN3,
+        ["BitLocker"] = ItemEnum.BITLOCKER,
         ["OpenCart"] = ItemEnum.OPENCART,
+        ["Whirlpool"] = ItemEnum.WHIRLPOOL,
         ["NetNTLMv2"] = ItemEnum.NET_NTLM_V2,
         ["AIX {ssha1}"] = ItemEnum.AIX_SSHA1,
-        ["Yescrypt $y$"] = ItemEnum.YESCRYPT,    // Hashcat Hash-Name NOT "scrypt [Bridged: Scrypt-Yescrypt]"
-        ["Whirlpool"] = ItemEnum.WHIRLPOOL,
+        ["Yescrypt $y$"] = ItemEnum.YESCRYPT,    // Hashcat Hash-Name IS NOT "scrypt [Bridged: Scrypt-Yescrypt]"
         ["FileVault 2"] = ItemEnum.FILEVAULT_2,
         ["AuthMe sha256"] = ItemEnum.AUTHME_SHA256,
+        ["Ansible Vault"] = ItemEnum.ANSIBLE_VAULT,
+        ["Mozilla key3.db"] = ItemEnum.MOZILLA_KEY3_DB,
+        ["Mozilla key4.db"] = ItemEnum.MOZILLA_KEY4_DB,
         ["GOST R 34.11-94"] = ItemEnum.GOST_R_34_11_94,
+        ["MultiBit HD (scrypt)"] = ItemEnum.MULTIBIT_HD_SCRYPT,
+        ["MultiBit Classic .key (MD5)"] = ItemEnum.MULTIBIT_CLASSIC_KEY_MD5,
+        ["MultiBit Classic .wallet (scrypt)"] = ItemEnum.MULTIBIT_CLASSIC_WALLET_SCRYPT,
         ["Base64 Encoded String"] = ItemEnum.BASE64,
         ["Password Safe v3 pwsafe3"] = ItemEnum.PASSWORDSAFE_V3, // Hashcat Hash-Name "Password Safe v3"
+        ["Android Backup"] = ItemEnum.ANDROID_BACKUP,
         ["Android FDE (Samsung DEK)"] = ItemEnum.ANDROID_FDE_SAMSUNG_DEK,
         ["WPA-PMKID-PBKDF2"] = ItemEnum.WPA_PMKID_PBKDF2,
+        ["WPA-PBKDF2-PMKID+EAPOL"] = ItemEnum.WPA_PBKDF2_PMKID_EAPOL,
         ["IPMI2 RAKP HMAC-SHA1"] = ItemEnum.IPMI2_RAKP_HMAC_SHA1,
         ["osCommerce, xt:Commerce"] = ItemEnum.OSCOMMERCE_XTCOMMERCE,
-        ["Blockchain, My Wallet"] = ItemEnum.BLOCKCHAIN_MY_WALLET,
-        ["Blockchain, My wallet, v2"] = ItemEnum.BLOCKCHAIN_MY_WALLET_V2,
         ["Cisco-IOS $9$ (scrypt)"] = ItemEnum.CISCO_IOS_SCRYPT,
         ["Cisco-IOS $8$ (PBKDF2-SHA256)"] = ItemEnum.CISCO_IOS_PBKDF2_SHA256,
+        ["Atlassian (PBKDF2-HMAC-SHA1)"] = ItemEnum.ATLASSIAN_PBKDF2_HMAC_SHA1,
+        ["Exodus Desktop Wallet (scrypt)"] = ItemEnum.EXODUS_DESKTOP_WALLET_SCRYPT,
+        ["BestCrypt v3 Volume Encryption"] = ItemEnum.BESTCRYPT_V3_VOLUME_ENCRYPTION,
         ["SMF (Simple Machines Forum) > v1.1"] = ItemEnum.SMF_AFTER_1_1,
-        ["Kerberos 5 AS-REQ Pre-Auth etype 23"] = ItemEnum.KERBEROS_5_AS_REQ_PRE_AUTH_ETYPE_23,
         ["Ruby on Rails Restful-Authentication"] = ItemEnum.RUBY_ON_RAILS_RESTFUL_AUTHENTICATION,
+        ["GPG (AES-128/AES-256 (SHA-1($plaintext)))"] = ItemEnum.GPG_AES_128_AES256_SHA1_PLAINTEXT,
+        ["KeePass 1 (AES/Twofish) and KeePass 2 (AES)"] = ItemEnum.KEEPASS1_AES_TWOFISH_KEEPASS2_AES,
+        ["Telegram Desktop >= v2.1.14 (PBKDF2-HMAC-SHA512)"] = ItemEnum.TELEGRAM_DESKTOP_AFTER_V2114_PBKDF_HMAC_SHA1,
+        ["PKCS#8 Private Keys (PBKDF2-HMAC-SHA1 + 3DES/AES)"] = ItemEnum.PKCS8_PRIVATE_KEYS_PBKDF2_HMAC_SHA1_3DES_AES,
+        ["PKCS#8 Private Keys (PBKDF2-HMAC-SHA256 + 3DES/AES)"] = ItemEnum.PKCS8_PRIVATE_KEYS_PBKDF2_HMAC_SHA256_3DES_AES,
     };
 }
