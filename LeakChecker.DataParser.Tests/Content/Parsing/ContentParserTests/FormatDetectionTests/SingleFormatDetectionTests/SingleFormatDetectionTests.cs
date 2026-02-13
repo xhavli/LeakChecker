670efa511dc7830bb1f57d7d6866f3cd23e2871e
Adam <xhavli59@stud.fit.vutbr.cz>
2026-02-13 19:11:05 +0100

    refactor: Path creation and simplify test case of SingleFormatDetectionTests

--- LeakChecker.DataParser.Tests/Content/Parsing/ContentParserTests/FormatDetectionTests/SingleFormatDetectionTests/SingleFormatDetectionTests.cs
+++ LeakChecker.DataParser.Tests/Content/Parsing/ContentParserTests/FormatDetectionTests/SingleFormatDetectionTests/SingleFormatDetectionTests.cs
@@ -17,25 +17,23 @@ public class SingleFormatDetectionTests
         DirectoryInfo? dir = new DirectoryInfo(Environment.CurrentDirectory);
         // *\LeakChecker
         dir = dir.Parent?.Parent?.Parent?.Parent;
-        _testDataDirectory = Path.Combine(
-            dir!.FullName,
-            "LeakChecker.DataParser.Tests/Content/Parsing/ContentParserTests/FormatDetectionTests/SingleFormatDetectionTests/Data");
+        _testDataDirectory = Path.Combine(dir!.FullName, "LeakChecker.DataParser.Tests/Data/SingleFormat");
     }
         
     [Theory]
-    [InlineData("Csv/Colon_Clean.txt", ':')]
-    [InlineData("Csv/Colon_Mess.txt", ':')]
-    [InlineData("Csv/Comma_Clean.txt", ',')]
-    [InlineData("Csv/Comma_Mess.txt", ',')]
-    [InlineData("Csv/Pipe_Clean.txt", '|')]
-    [InlineData("Csv/Pipe_Mess.txt", '|')]
-    [InlineData("Csv/Semicolon_Clean.txt", ';')]
-    [InlineData("Csv/Semicolon_Mess.txt", ';')]
-    [InlineData("Csv/Space_Clean.txt", ' ')]
-    [InlineData("Csv/Space_Mess.txt", ' ')]
-    [InlineData("Csv/Tab_Clean.txt", '\t')]
-    [InlineData("Csv/Tab_Mess.txt", '\t')]
-    public async Task ShouldDetect_SingleCsvWithDelimiter(string fileName, char expectedDelimiter)
+    [InlineData("Csv/Colon_Clean.txt")]
+    [InlineData("Csv/Colon_Messy.txt")]
+    [InlineData("Csv/Comma_Clean.txt")]
+    [InlineData("Csv/Comma_Messy.txt")]
+    [InlineData("Csv/Pipe_Clean.txt")]
+    [InlineData("Csv/Pipe_Messy.txt")]
+    [InlineData("Csv/Semicolon_Clean.txt")]
+    [InlineData("Csv/Semicolon_Messy.txt")]
+    [InlineData("Csv/Space_Clean.txt")]
+    [InlineData("Csv/Space_Messy.txt")]
+    [InlineData("Csv/Tab_Clean.txt")]
+    [InlineData("Csv/Tab_Messy.txt")]
+    public async Task ShouldDetect_SingleCsv(string fileName)
     {
         // Arrange
         string filePath = Path.Combine(_testDataDirectory, fileName);
@@ -47,18 +45,18 @@ public class SingleFormatDetectionTests
 
         // Assert
         Assert.Single(stats.Formats);
+        Assert.Single(stats.Delimiters);
         Assert.Equal(FormatEnum.Csv, stats.Formats.First());
-        Assert.Equal(expectedDelimiter, stats.Delimiters.First());
     }
     
     [Theory]
     [InlineData("SqlInsert/MySQL_Clean.txt")]
-    [InlineData("SqlInsert/MySQL_Mess.txt")]
+    [InlineData("SqlInsert/MySQL_Messy.txt")]
     [InlineData("SqlInsert/PostgreSQL_Clean.txt")]
-    [InlineData("SqlInsert/PostgreSQL_Mess.txt")]
+    [InlineData("SqlInsert/PostgreSQL_Messy.txt")]
     [InlineData("SqlInsert/SQLServer_Clean.txt")]
-    [InlineData("SqlInsert/SQLServer_Mess.txt")]
-    public async Task ShouldDetect_SingleSqlInsertWithDelimiter(string fileName)
+    [InlineData("SqlInsert/SQLServer_Messy.txt")]
+    public async Task ShouldDetect_SingleSqlInsert(string fileName)
     {
         // Arrange
         string filePath = Path.Combine(_testDataDirectory, fileName);
@@ -72,6 +70,5 @@ public class SingleFormatDetectionTests
         Assert.Single(stats.Formats);
         Assert.Single(stats.Delimiters);
         Assert.Equal(FormatEnum.SqlInsert, stats.Formats.First());
-        Assert.Equal(',', stats.Delimiters.First());
     }
 }
