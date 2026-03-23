a9babfa99b8bad89495884eaf6724dbb3b0fee8a
Adam <xhavli59@stud.fit.vutbr.cz>
2026-03-23 01:18:57 +0100

    feat: ArchiveExtractor parallelization init

--- LeakChecker.DataParser/Utilities/ArchiveExtraction/ArchiveExtractor.cs
+++ LeakChecker.DataParser/Utilities/ArchiveExtraction/ArchiveExtractor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using LeakChecker.DataParser.Logging;
 using LeakChecker.DataParser.Logging.Execution;
 using LeakChecker.DataParser.Utilities.Settings;
@@ -11,42 +12,46 @@ public sealed class ArchiveExtractor(ISettings settings, ExecutionLogger logger)
 
     public async Task<IEnumerable<string>> ExtractArchives(IEnumerable<string> inputPaths)
     {
-        var extractor = new Extractor();
-        HashSet<string> parsePaths = new();
+        var parsePaths = new ConcurrentDictionary<string, byte>();
         string extractionRoot = Path.GetFullPath(Path.Combine(settings.TmpDirectory, "Extracted") + Path.DirectorySeparatorChar);
         
-        foreach (var path in inputPaths)
-        {
-            foreach (var entry in extractor.Extract(path))
+        await Parallel.ForEachAsync(
+            inputPaths,
+            new ParallelOptions { MaxDegreeOfParallelism = settings.ThreadsCapacity },
+            async (path, ct) =>
             {
-                if (entry.Parent == null)
+                Extractor extractor = new();
+                
+                foreach (var entry in extractor.Extract(path))
                 {
-                    // Not an archive
-                    parsePaths.Add(path);
-                    continue;
-                }
-                
-                string relativeArchivePath = GetRelativeArchivePath(path, entry.FullPath);
-                string dstPath = Path.GetFullPath(Path.Combine(extractionRoot, relativeArchivePath));
+                    if (entry.Parent == null)
+                    {
+                        // Not an archive
+                        parsePaths.TryAdd(path, 0);
+                        continue;
+                    }
+                    
+                    string relativeArchivePath = GetRelativeArchivePath(path, entry.FullPath);
+                    string dstPath = Path.GetFullPath(Path.Combine(extractionRoot, relativeArchivePath));
 
-                if (!dstPath.StartsWith(extractionRoot, StringComparison.Ordinal))
-                {
-                    await logger.Log($"Potential Zip Slip. Archive entry tries resolve outside extraction root: '{entry.FullPath}'", LogLevel.Warning);
-                    continue;
-                }
-                
-                string? directory = Path.GetDirectoryName(dstPath);
-                if (!string.IsNullOrEmpty(directory))
-                    Directory.CreateDirectory(directory);
+                    if (!dstPath.StartsWith(extractionRoot, StringComparison.Ordinal))
+                    {
+                        await logger.Log($"Potential Zip Slip. Archive entry tries resolve outside extraction root: '{entry.FullPath}'", LogLevel.Warning);
+                        continue;
+                    }
+                    
+                    string? directory = Path.GetDirectoryName(dstPath);
+                    if (!string.IsNullOrEmpty(directory))
+                        Directory.CreateDirectory(directory);
 
-                await using var outStream = new FileStream(dstPath, FileMode.Append, FileAccess.Write);
-                await entry.Content.CopyToAsync(outStream);
-                
-                parsePaths.Add(dstPath);
-            }
-        }
+                    await using var outStream = new FileStream(dstPath, FileMode.Append, FileAccess.Write);
+                    await entry.Content.CopyToAsync(outStream, ct);
+                    
+                    parsePaths.TryAdd(dstPath, 0);
+                }
+            });
         
-        return parsePaths;
+        return parsePaths.Keys;
     }
     
     private static string GetRelativeArchivePath(string sourcePath, string entryFullPath)
