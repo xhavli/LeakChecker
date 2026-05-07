b531f337285c2a07e14b50ebfb7aa5be843ba5f6
Adam <xhavli59@stud.fit.vutbr.cz>
2026-05-08 01:21:32 +0200

    feat: EncodingConverter proper fix of invalid bytes

--- LeakChecker.DataParser/Encodings/Conversion/EncodingConverter.cs
+++ LeakChecker.DataParser/Encodings/Conversion/EncodingConverter.cs
@@ -10,8 +10,11 @@ public static class EncodingConverter
 {
     private static readonly string Utf8Name = Encoding.UTF8.WebName;
     private static readonly string AsciiName = Encoding.ASCII.WebName;
-    private static readonly DecoderReplacementFallback ReplacementFallback = new(string.Empty); // Skip invalid bytes
-    private static readonly Encoding Utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: false);
+    private static readonly DecoderReplacementFallback FallbackDrop = new(string.Empty); // Drop invalid bytes
+    
+    public static readonly Encoding Utf8 = Encoding.GetEncoding(Utf8Name, 
+        encoderFallback: new EncoderReplacementFallback(string.Empty),  // Drop unencodable chars
+        decoderFallback: new DecoderReplacementFallback(string.Empty)); // Drop invalid bytes
 
     /// <summary>
     /// Converts a file with mixed encodings into a single UTF-8 encoded output file.
@@ -47,7 +50,7 @@ public static class EncodingConverter
             long remaining = segment.Length;
             byte[] buffer = new byte[bufferSize];
             Decoder decoder = segment.Encoding.GetDecoder();
-            decoder.Fallback = ReplacementFallback;
+            decoder.Fallback = FallbackDrop;
             char[] charBuffer = new char[segment.Encoding.GetMaxCharCount(bufferSize)];
 
             while (remaining > 0)
